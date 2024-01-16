87311f74848b2307700901c1f33455e3d4b368dc
Asher Winstead <111993154+MrAshCreates@users.noreply.github.com>
2024-01-16 00:51:32 -0600

    Update bert.js

--- scripts/bert.js
+++ scripts/bert.js
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bertstyle.css">
+    <link rel="stylesheet" href="./bertstyle.css">
     <title>User Profile</title>
 </head>
 <body>
@@ -16,6 +16,6 @@
             <img src="instagram-logo.png" alt="Instagram Logo" class="instagram-logo">
         </a>
     </div>
-    <script src="bertscript.js"></script>
+    <script src="./bertscript.js"></script>
 </body>
 </html>
